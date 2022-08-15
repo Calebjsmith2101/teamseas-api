1ba57417213b261c4ca6fb8dfbff492718472665
Caleb Smith <calebjsmith2101@gmail.com>
2022-08-15 15:12:46 +1000

    'commit'

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { GraphQLModule } from '@nestjs/graphql'
+import { Module } from '@nestjs/common';
+import { DonationsModule } from './donations/donations.module';
 @Module({
-  imports: [],
+  imports: [GraphQLModule.forRoot({
+    typePaths: ['./**/*.graphql'],
+    playground: false,
+    plugins: [ApolloServerPluginLandingPageLocalDefault]
+  }), DonationsModule,
+],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/donations/donations.service.ts
@@ -0,0 +1,18 @@
+import { CreateDonationInput } from './dto/create-donation.input';
+import { Injectable } from '@nestjs/common';
+import { UpdateDonationInput } from './dto/update-donation.input';
+
+@Injectable()
+export class DonationsService {
+  create(createDonationInput: CreateDonationInput) {
+    return 'This action adds a new donation';
+  }
+
+  findAll() {
+    return `This action returns all donations`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} donation`;
+  }
+}
