918fc8e2b1d42c1ffb4dbef0663886edbd42a0c0
Caleb Smith <calebjsmith2101@gmail.com>
2022-08-16 14:21:42 +1000

    'commit'

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,22 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DonationsModule } from './donations/donations.module';
+import { GraphQLDateTime } from 'graphql-iso-date';
 import { GraphQLModule } from '@nestjs/graphql'
 import { Module } from '@nestjs/common';
-import { DonationsModule } from './donations/donations.module';
+
 @Module({
-  imports: [GraphQLModule.forRoot({
-    typePaths: ['./**/*.graphql'],
+  imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
+    driver: ApolloDriver,
     playground: false,
-    plugins: [ApolloServerPluginLandingPageLocalDefault]
-  }), DonationsModule,
+    plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    typePaths: ['./**/*.graphql'],
+    resolvers: { DateTime: GraphQLDateTime },
+  }),
+  DonationsModule,
 ],
   controllers: [AppController],
   providers: [AppService],
